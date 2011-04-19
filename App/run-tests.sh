b3b66da16a7adcf2af0388b523fce2a8aa43b6c9
Wolfger Schramm <wolfger@spearwolf.de>
2011-04-19 15:27:30 +0200

    run test suite with headless phantomjs webkit/qt engine

---
+++ App/run-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#------------------
+# File: run-tests.sh
+# Author: Wolfger Schramm <wolfger@spearwolf.de>
+# Created: 19.04.2011 15:23:56 CEST
+
+phantomjs test/testrunner.js file://`pwd`/test/index.html
